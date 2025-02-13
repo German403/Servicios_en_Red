9d4f246a231999e6067ccbdc74c7a607aa3ecae8
gescd <gescuderodriguez@gmail.com>
2025-02-13 03:06:12 +0100

    Initial commit

---
+++ src/Pruebas/ClienteFTP1.java
@@ -0,0 +1,32 @@
+package Pruebas;
+
+import java.io.*;
+import java.net.*;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+
+public class ClienteFTP1 {
+    public static void main(String[] args) throws SocketException, IOException {
+        FTPClient clienetFTP = FTPClient();
+        String servidorFTP = "ftp.rediris.es";
+        System.out.println("Conexion a: " + servidorFTP);
+
+        clienetFTP.connect(servidorFTP);
+        clienetFTP.enterLocalPassiveMode();
+
+        System.out.println(clienetFTP.getReplyString());
+        int respuesta = clienetFTP.getReplyCode();
+
+        System.out.println("Respuesta: " + respuesta);
+
+        if(!FTPReply.isPositiveCompletion(respuesta)) {
+            clienetFTP.disconnect();
+            System.out.println("Conexion rechazada: " + respuesta);
+            System.exit(0);
+        }
+
+        clienetFTP.disconnect();
+        System.out.println("Conexion finalizada");
+    }
+}
